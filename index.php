86cd736d27f7da7f8a1858c9d02f2b89d08ab8e1
TSGGS <sagioatscie@gmail.com>
2022-01-14 19:55:19 +0800

    added design

--- index.php
+++ index.php
@@ -9,6 +9,18 @@
         <h1 class="welcome-text index">WALANG TATAK</h1>
         <h3 class="welcome-subtext">masarap kahit... walang tatak</h3>
     </div>
+    <div class="types">
+        <img class="type-image" src="images/resources/cookies.jpg" alt="cookies">
+        <span class="type-name" style="top: 0; left: 0; padding-top: 1px; padding-left: 35px;">COOKIES</span>
+    </div>
+    <div class="types">
+        <img class="type-image" src="images/resources/crinkles.jpeg" alt="cookies">
+        <span class="type-name" style="top: 70%; left: 0; padding-left: 35px;">CRINKLES</span>
+    </div>
+    <div class="types">
+        <img class="type-image" src="images/resources/cakes.jpg" alt="cookies">
+        <span class="type-name" style="top: 0; left: 72%; padding-top: 1px; padding-right: 35px;">CAKES</span>
+    </div>
 
 <?php
     require("templates/footer.php");
