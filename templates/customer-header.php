c9d793332fc06366ea1677799bf6db7e44543464
TSGGS <sagioatscie@gmail.com>
2022-01-17 09:47:44 +0800

    update product.php

--- templates/customer-header.php
+++ templates/customer-header.php
@@ -2,11 +2,11 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/customer-header.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     </head>
 
     <body>
@@ -14,7 +14,7 @@
             <img class="logo" src="images/resources/logo.jpg" alt="logo">
             <ul class="nav-area">
                 <li><a href="index.php">Home</a></li>
-                <li><a href="#">Order</a></li>
+                <li><a href="product.php">Order</a></li>
                 <!-- <li><a href="contact.php">Contact</a></li> -->
             </ul>
         </div>
