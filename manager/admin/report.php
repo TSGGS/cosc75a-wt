6471726bff0704a1b7966b714e8589fcca6072eb
TSGGS <sagioatscie@gmail.com>
2022-08-30 12:48:17 +0800

    updated

--- manager/admin/report.php
+++ manager/admin/report.php
@@ -4,9 +4,88 @@
 
     changeTitle("../templates/header.php", "Adminitstrator - WT");
     validateUserPage($_SESSION["tid"], $_SERVER["REQUEST_URI"]);
+
+    // Get Total
+    $sql = "SELECT SUM(order_total_price) as SUM FROM orders WHERE order_timestamp BETWEEN DATE_FORMAT(CURRENT_DATE()-6, '%Y-%m-%d') AND CURRENT_TIMESTAMP();";
+    $mysql_result = prepareSQL($conn, $sql);
+    $result = mysqli_fetch_array($mysql_result);
+    
+    // Get Hero Product
+    $sql2 = "SELECT products.product_name AS Name, SUM(order_items.order_product_quantity) AS SUM FROM `order_items` LEFT JOIN orders ON order_items.order_id = orders.order_id LEFT JOIN products ON order_items.order_product_id=products.product_id WHERE orders.order_timestamp BETWEEN DATE_FORMAT(CURRENT_DATE()-6, '%Y-%m-%d') AND CURRENT_TIMESTAMP() AND orders.order_status_id=2 GROUP BY order_items.order_product_id ORDER BY SUM DESC;";
+    $mysql_result2 = $conn->query($sql2);
+
+    //Get Sold Products
+    $sql3 = "SELECT products.product_name AS Name, SUM(order_items.order_product_quantity) AS SUM FROM `order_items` LEFT JOIN orders ON order_items.order_id = orders.order_id LEFT JOIN products ON order_items.order_product_id=products.product_id WHERE orders.order_timestamp BETWEEN DATE_FORMAT(CURRENT_DATE()-6, '%Y-%m-%d') AND CURRENT_TIMESTAMP() AND orders.order_status_id=2 GROUP BY order_items.order_product_id ORDER BY SUM DESC;";
+    $mysql_result3 = $conn->query($sql3);
+    
+    // Get Inventory
+    $sql4 = "SELECT products.product_name AS Name, inventory.inventory_product_count AS qty FROM inventory INNER JOIN products ON inventory_id=products.product_id;";
+    $mysql_result4 = $conn->query($sql4);
 ?>
-    <div id="content">
-        <h1>Generate Report</h1>
+
+    <div class="container mt-3">
+        <h1>Week Report</h1>
+        <div class="row mt-4">
+            <div class="col-3 border-hidden"><h3 class="align-bottom">Total Sales</h3></div>
+            <div class="col border"><h3><?= "Php ".$result["SUM"] ?></h3></div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-3 border-hidden"><h3 class="align-bottom">Hero Product</h3></div>
+            <div class="col border">
+                <?php
+                    $highest = 0;
+                    while($result = mysqli_fetch_array($mysql_result2)) {
+                        if($result["SUM"] >= $highest) {
+                            echo "<h3>".$result["Name"]."<br></h3>";
+                            $highest = $result["SUM"];
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+        <h3 class="mt-4">Ordered Products</h3>
+        <table class="table table-sm" style="text-align: center;">
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Sold Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while($result = mysqli_fetch_array($mysql_result3)) {
+                        echo '
+                            <tr>
+                                <td><h5>'.$result["Name"].'</h5></td>
+                                <td><h5>'.$result["SUM"].'</h5></td>
+                            </tr>
+                        ';
+                    }
+                ?>
+            </tbody>
+        </table>
+        <h3 class="mt-4">Inventory</h3>
+        <table class="table table-sm" style="text-align: center;">
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Remaining Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while($result = mysqli_fetch_array($mysql_result4)) {
+                        echo '
+                            <tr>
+                                <td><h5>'.$result["Name"].'</h5></td>
+                                <td><h5>'.$result["qty"].'</h5></td>
+                            </tr>
+                        ';
+                    }
+                ?>
+            </tbody>
+        </table>
     </div>
+
 <?php
-    require("../templates/footer.php");
+    require("../templates/footer.php"); 
--- manager/marketing/dashboard.php
+++ manager/marketing/dashboard.php
@@ -5,8 +5,85 @@
     changeTitle("../templates/header.php", "Marketing - WT");
     validateUserPage($_SESSION["tid"], $_SERVER["REQUEST_URI"]);
 ?>
-    <div id="content">
-        Marketing Dashboard
+    <div class="content mt-3">
+        <div class="row mx-3">
+            <div class="col mx-3">
+                <h1>Promotions</h1>
+                <table class="table" style="text-align: center;">
+                    <thead>
+                        <tr>
+                            <th>Promotion Code</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th>Image</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT promotion_code, promotion_start_timestamp, promotion_end_timestamp, promotion_image FROM promotions WHERE promotion_start_timestamp > current_timestamp() AND promotion_end_timestamp > current_timestamp() OR promotion_start_timestamp < current_timestamp() AND promotion_end_timestamp > current_timestamp()";
+                            $promotion = prepareSQL($conn, $sql);
+                            
+                            if(mysqli_num_rows($promotion) > 0) {
+                                while($result = mysqli_fetch_array($promotion)) {
+                                    echo'
+                                        <tr>
+                                            <td>'.$result["promotion_code"].'</td>
+                                            <td>'.$result["promotion_start_timestamp"].'</td>
+                                            <td>'.$result["promotion_end_timestamp"].'</td>
+                                            <td><a href="../../images/promotions/'.$result["promotion_image"].'" target="_blank">View</a></td>
+                                        </tr>
+                                    ';
+                                }
+                            } else {
+                                    echo'
+                                        <tr>
+                                            <td colspan="4">There are no promotions running</td>
+                                        </tr>
+                                    ';
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col mx-3">
+                <h1>Discounts</h1>
+                <table class="table" style="text-align: center;">
+                    <thead>
+                        <tr>
+                            <th>Discount Code</th>
+                            <th>Percentage</th>
+                            <th>Start</th>
+                            <th>End</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT discount_code, discount_amount, discount_start_timestamp, discount_end_timestamp FROM discounts WHERE discount_start_timestamp < CURRENT_TIMESTAMP() AND discount_end_timestamp > CURRENT_TIMESTAMP()";
+                            $discount = prepareSQL($conn, $sql);
+                            
+                            if(mysqli_num_rows($discount) > 0) {
+                                while($result = mysqli_fetch_array($discount)) {
+                                    echo'
+                                        <tr>
+                                            <td>'.$result["discount_code"].'</td>
+                                            <td>'.$result["discount_amount"].'%</td>
+                                            <td>'.$result["discount_start_timestamp"].'</td>
+                                            <td>'.$result["discount_end_timestamp"].'</td>
+                                        </tr>
+                                    ';
+                                }
+                            } else {
+                                    echo'
+                                        <tr>
+                                            <td colspan="4">There are no discounts running</td>
+                                        </tr>
+                                    ';
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 <?php
     require("../templates/footer.php");
